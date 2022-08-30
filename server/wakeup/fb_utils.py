9f14a82e8b37b933f6a8378566dcb15e69b17d68
Rasmus Antons <mail@rasmusantons.de>
2022-08-30 23:23:08 +0200

    catch unregistered device errors

--- server/wakeup/fb_utils.py
+++ server/wakeup/fb_utils.py
@@ -11,7 +11,7 @@ def send_alarm_to_device(device, alarm):
     fb_message = messaging.Message(data={'alarm': str(alarm.id), 'message': alarm.message or ''}, token=device.fb_token)
     try:
         messaging.send(fb_message)
-    except firebase_admin.exceptions.InvalidArgumentError:
+    except (firebase_admin.exceptions.InvalidArgumentError, firebase_admin._messaging_utils.UnregisteredError):
         device.fb_token = None
         device.save()
         return False
